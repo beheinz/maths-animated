ed5cffc6fe6dd0861e786dfab9bd709eb52af3b7
beheinz <bhynes34@gmail.com>
2020-07-21 20:48:58 +0100

    Added click functionality to homepage

--- sketch.js
+++ sketch.js
@@ -1,9 +1,7 @@
-let time_in_b = 0;
-let time = 0;
-let angleDegrees = 90;
-
 // an array to add multiple particles
 let particles = [];
+
+var mouse_been_down = 0;
 
 function preload() {
   fontReg = loadFont('/../../assets/Poppins-Regular.ttf');
@@ -14,10 +12,10 @@ function setup() {
   textFont(fontReg);
   imageMode(CENTER);
   rectMode(CENTER, CENTER);
-  frameRate(20);
+  frameRate(25);
 
   // particles
-  for(let i = 0;i<70;i++){
+  for(let i = 0;i<60;i++){
     particles.push(new Particle());
   }
 }
@@ -25,26 +23,41 @@ function setup() {
 function draw() {
 
   background('black');
-
   textAlign(CENTER, CENTER);
-  fill('white');
-  //image(img, width*0.5, height*0.4, width*0.4, width*0.4*0.39863);
-  stroke('white');
-  noFill();
-
 
   for(let i = 0;i<particles.length;i++) {
     particles[i].createParticle();
     particles[i].moveParticle();
     particles[i].joinParticles(particles.slice(i));
+  }
+
+  textSize(24);
+  noStroke();
+  fill('rgba(200,169,169,0.3)');
+
+  if (mouse_been_down == 0 & millis() > 3000) {
+  text("Click the background", 0.5*width, 0.8*height);
 }
 
+
+  if (mouseIsPressed) {
+    // create new particle
+    particles.push(new Particle());
+    particles[particles.length-1].setLocation();
+
+    // remove oldest particle
+    particles.splice(0,1);
+
+    // log mouse press
+    mouse_been_down = 1;
+  }
 }
 
 // this class describes the properties of a single particle.
 class Particle {
-// setting the co-ordinates, radius and the
-// speed of a particle in both the co-ordinates axes.
+
+  // setting the co-ordinates, radius and the
+  // speed of a particle in both the co-ordinates axes.
   constructor(){
     this.x = random(0,width);
     this.y = random(0,height);
@@ -53,14 +66,20 @@ class Particle {
     this.ySpeed = random(-this.r/5, this.r/5);
   }
 
-// creation of a particle.
+  // set location to mouse location for new particles
+  setLocation(){
+    this.x = mouseX;
+    this.y = mouseY;
+  }
+
+  // creation of a particle.
   createParticle() {
     noStroke();
     fill('rgba(200,169,169,0.5)');
     circle(this.x,this.y,this.r);
   }
 
-// setting the particle in motion.
+  // setting the particle in motion.
   moveParticle() {
     if(this.x < 0 || this.x > width)
       this.xSpeed*=-1;
@@ -70,13 +89,17 @@ class Particle {
     this.y+=this.ySpeed;
   }
 
-// this function creates the connections(lines)
-// between particles which are less than a certain distance apart
+  // this function creates the connections(lines)
+  // between particles which are less than a certain distance apart
   joinParticles(paraticles) {
     particles.forEach(element =>{
       let dis = dist(this.x,this.y,element.x,element.y);
-      if(dis<120) {
-        stroke('rgba(255,255,255,0.08)');
+      if(dis<120 && dis>80) {
+        stroke('rgba(255,255,255,0.1)');
+        line(this.x,this.y,element.x,element.y);
+      }
+      if(dis<80) {
+        stroke('rgba(255,255,255,0.2)');
         line(this.x,this.y,element.x,element.y);
       }
     });
